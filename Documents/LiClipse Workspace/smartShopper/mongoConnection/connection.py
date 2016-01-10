16e125582a4c043b1674f1edaa75765dea5bc9b1
upagupta2003 <upa.gupta@mavs.uta.edu>
2016-01-09 22:45:14 -0600

    predicted and inserted result to db

--- Documents/LiClipse Workspace/smartShopper/mongoConnection/connection.py
+++ Documents/LiClipse Workspace/smartShopper/mongoConnection/connection.py
@@ -1,47 +1,51 @@
-'''
-Created on Dec 30, 2015
-
-@author: ugupta
-'''
-import json
 from pymongo import MongoClient
+import datetime
 
 client = MongoClient()
 
 client =  MongoClient('mongodb://admin:abcd1234@ds043002.mongolab.com:43002/sgm_demo')
 
+
 def linear_regression(x, y):
-    #x --> number days between two purchase date. An array
-    #y --> number of items purchased. An array
     length = len(x)
     sum_x = sum(x)
     sum_y = sum(y)
-
- 
+    
+    
     sum_x_squared = sum(map(lambda a: a * a, x))
     sum_of_products = sum([x[i] * y[i] for i in range(length)])
-
+    
     a = (sum_of_products - (sum_x * sum_y) / length) / (sum_x_squared - ((sum_x ** 2) / length))
     b = (sum_y - a * sum_x) / length
     return a, b
 
-
-
+def datetime2daysindifference(x):
+    no_days = len(x)
+    #print("num of days: "+ str(no_days))
+    diff_dates = []
+    diff_dates.append(0)
+    for i in range(0, no_days-1):
+        date1 = x[i]
+        date2 = x[i+1]
+        diff = date2 - date1
+        second_diff = diff.total_seconds()
+        day_diff =  int(second_diff/(24*60*60))
+        diff_dates.append(int(day_diff))
+    #print(diff_dates)
+    return diff_dates
+ 
 if __name__ == '__main__':
     db = client.sgm_demo
     
     all_user =  db.user.find({},{"_id":1})
     
     all_item = db.item_details.find({},{"_id":1})
-    
-    purchase_dates = []
-    quantity = []
-    
+
     items = []
+    
     '''
     for item in all_item:
-    
-        for user in all_user:
+            for user in all_user:
             #printing  users
             print(user)
             #'listbought':True, 'user':user['_id'], 'listitems.itemref':item['_id']
@@ -51,29 +55,48 @@ if __name__ == '__main__':
                 
                 print(g['listitems.itemref'])
                 
-    '''
-    
+    '''  
+    itemid = []  
+    userItems = {}     
+
     for item in all_item:
-        #print(item["_id"])
+        itemid.append(item["_id"])
+          
+    for user in all_user:
+        items_purchased = []
+        #printing  users
+        #print(user)
+        itemref = []
+        all_purchsed_grocery = db.grocery_list.find({'listbought':True, 'user':user['_id']})    
+        item_bought = []
+        days_bought = []
+        quantity = []
+        for grocery_list in all_purchsed_grocery:
+            for items in grocery_list["listitems"]:
+                item_bought.append(items["itemref"])
+                days_bought.append(items["bought_at"])
+                quantity.append(items["quantity"])
+        #print(item_bought)
+        #print(days_bought)
+        #print(quantity)
         
-        for user in all_user:
-            items_purchased = []
-            
-            #printing  users
-            print(user)
-            #'listbought':True, 'user':user['_id'], 'listitems.itemref':item['_id']
-            all_purchsed_grocery = db.grocery_list.find({'listbought':True, 'user':user['_id']})
-            
-            for grocery_list in all_purchsed_grocery:
-                items_purchased.append(grocery_list['listitems'])
+        for j in itemid:
+            date_bought = []
+            quant = []
+            for k in range(0, len(item_bought)):
+                if j == item_bought[k]:
+                    date_bought.append(days_bought[k])
+                    quant.append(quantity[k])
+            if len(date_bought) != 0:
+                diff_days = datetime2daysindifference(date_bought)
+                a,b = linear_regression(diff_days, quant)
+                print(a,b)
+                date = date_bought[len(date_bought)-1] + datetime.timedelta(days=a)
+                print(date)
+                previous_prediction = db.predictions.find({'userid':user , 'itemref':j})
+                if previous_prediction.count() == 0:
+                    predict = ({"userid":user, "itemref":j, "next_date":date, "predict_quantity":b , "lastModified": datetime.datetime.now()} )
+                    db.predictions.insert(predict)
+                else:
+                    db.inventory.update({ "userid": user, "itemref":j },{ "$set": {"next_date": date, "predict_quantity":b}, "$currentDate": { "lastModified":True}})
                 
-                for items in items_purchased:
-                    for i in items:
-                        if i["itemref"] == item["_id"]:
-                            print("found")
-        
-        
-        
-        
-    
-    
